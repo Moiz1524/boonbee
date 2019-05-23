b444a6af1bcd32507d4a8c40a56b339e82408b9b
Imran Hayat <imran.hayat92@gmail.com>
2019-05-23 00:47:55 +0000

    Setting up database migrations

--- db/migrate/20190523004546_add_job_flag_to_campaigns.rb
+++ db/migrate/20190523004546_add_job_flag_to_campaigns.rb
@@ -1,6 +1,5 @@
 class AddJobFlagToCampaign < ActiveRecord::Migration[5.2]
   def change
     add_column :campaigns, :job_flag, :boolean, :default => false
-    remove_column :campaigns, :jobFlag
   end
 end
