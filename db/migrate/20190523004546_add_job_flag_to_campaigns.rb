c9d0d848414b01007f44ce68b42648eee93574ab
Imran Hayat <imran.hayat92@gmail.com>
2019-05-23 00:50:43 +0000

    Fixing migration errors

--- db/migrate/20190523004546_add_job_flag_to_campaigns.rb
+++ db/migrate/20190523004546_add_job_flag_to_campaigns.rb
@@ -1,4 +1,4 @@
-class AddJobFlagToCampaign < ActiveRecord::Migration[5.2]
+class AddJobFlagToCampaigns < ActiveRecord::Migration[5.2]
   def change
     add_column :campaigns, :job_flag, :boolean, :default => false
   end
